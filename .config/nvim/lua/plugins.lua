54bd3d424cb8f01c5655fd089915341482b2b322
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-02-25 22:03:05 +0200

    nvim: Move all configuration into plugins file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -71,7 +71,51 @@ return require('packer').startup(function(use)
 
 	use {
 		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
+		run = ':TSUpdate',
+		config = function()
+			require 'nvim-treesitter.configs'.setup {
+				-- A list of parser names, or "all" (the four listed parsers should always be installed)
+				ensure_installed = { "go", "python", "lua", "vim", "help" },
+
+				-- Install parsers synchronously (only applied to `ensure_installed`)
+				sync_install = false,
+
+				-- Automatically install missing parsers when entering buffer
+				-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+				auto_install = true,
+
+				-- List of parsers to ignore installing (for "all")
+				--ignore_install = { "javascript" },
+
+				---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+				-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+				highlight = {
+					-- `false` will disable the whole extension
+					enable = true,
+
+					-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+					-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+					-- the name of the parser)
+					-- list of language that will be disabled
+					--disable = { "c", "rust" },
+					-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+					disable = function(lang, buf)
+						local max_filesize = 100 * 1024 -- 100 KB
+						local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+						if ok and stats and stats.size > max_filesize then
+							return true
+						end
+					end,
+
+					-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+					-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+					-- Using this option may slow down your editor, and you may see some duplicate highlights.
+					-- Instead of true it can also be a list of languages
+					additional_vim_regex_highlighting = false,
+				},
+			}
+		end
 	}
 
 	use 'mbbill/undotree'
@@ -112,6 +156,29 @@ return require('packer').startup(function(use)
 			-- Snippets
 			{ 'L3MON4D3/LuaSnip' }, -- Required
 			{ 'rafamadriz/friendly-snippets' }, -- Optional
-		}
+		},
+		config = function()
+			local lsp = require('lsp-zero')
+			lsp.preset('recommended')
+
+			lsp.ensure_installed({
+				'gopls',
+				'lua_ls',
+			})
+
+			lsp.on_attach(function(client, bufnr)
+				local opts = { buffer = bufnr, remap = false }
+
+				vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+				vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+				vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
+
+				-- auto format on save, formatting_sync instead of formatting because
+				-- formatting is async
+				vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+			end)
+
+			lsp.setup()
+		end
 	}
 end)
