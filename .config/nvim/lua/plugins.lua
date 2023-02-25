b7e8a069981ef13718cd036c878f1722f66f88ca
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-02-26 00:41:22 +0200

    nvim: Add comment

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -104,6 +104,7 @@ return require('packer').startup(function(use)
 	use {
 		"tpope/vim-fugitive",
 		config = function()
+			-- https://github.com/tpope/vim-fugitive/issues/1237#issuecomment-475005345
 			vim.keymap.set("n", "<leader>gs", ":vertical Git<CR>")
 			vim.keymap.set("n", "<leader>gl", ":vertical Git log<CR>")
 			vim.opt.splitright = true
