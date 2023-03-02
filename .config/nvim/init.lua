b836f1ab3ea97ba0759efce7ec3a79d8b9221249
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-03-02 11:05:00 +0200

    nvim: Add lsp keybindings

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -178,6 +178,9 @@ return require('packer').startup(function(use)
 				vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 				vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 				vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
+				vim.keymap.set("n", "<leader>e", function() vim.diagnostic.open_float() end, opts)
+				vim.keymap.set('n', '[d', function() vim.diagnostic.goto_prev() end, opts)
+				vim.keymap.set('n', ']d', function() vim.diagnostic.goto_next() end, opts)
 
 				-- auto format on save, formatting_sync instead of formatting because
 				-- formatting is async
