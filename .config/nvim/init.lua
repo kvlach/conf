c8f6336bbbfb1f3aae3f8473ae1ea66464879db6
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-03-12 17:45:06 +0200

    nvim: Add file tree plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -204,4 +204,33 @@ return require('packer').startup(function(use)
 			require("nvim-autopairs").setup {}
 		end
 	}
+
+	use {
+		'nvim-tree/nvim-tree.lua',
+		--requires = {
+		--	'nvim-tree/nvim-web-devicons', -- optional, for file icons
+		--},
+		config = function()
+			-- says this in the docs:
+			-- disable netrw at the very start of your init.lua (strongly advised)
+			--vim.g.loaded_netrw = 1
+			--vim.g.loaded_netrwPlugin = 1
+
+			require("nvim-tree").setup({
+				renderer = {
+					icons = {
+						show = {
+							file = false,
+							folder = false,
+							folder_arrow = false,
+							git = false,
+							modified = false,
+						},
+					},
+				},
+			})
+
+			vim.keymap.set("n", "<leader>t", vim.cmd.NvimTreeToggle)
+		end
+	}
 end)
