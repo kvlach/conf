88797526eba4b69b2b930b75ec0d8b3f7f3245f2
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-03-12 14:53:17 +0200

    nvim: Change undodir location

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,7 +8,7 @@ vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = vim.fn.stdpath("state") .. "/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
