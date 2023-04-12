62bc19ec479466c1903fd548639973c82bce8d3a
Konstantinos Vlachopoulos <kon@vlach.gr>
2023-04-12 16:42:20 +0300

    nvim: Add keybinds for system clipboard

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -25,6 +25,10 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+y')
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
